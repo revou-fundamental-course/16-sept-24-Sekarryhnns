e3f5892a36ad39427f317e7ed1e56603f12aa58b
Sekarryhnns <sekarrayhannisa.com>
2024-09-24 19:46:41 +0700

    Selesai.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 /*ini js*/
+
+// Toggle menu saat hamburger icon diklik
+const hamburger = document.querySelector('.hamburger');
+const navBar = document.querySelector('.nav-bar');
+
+hamburger.addEventListener('click', function () {
+    navBar.classList.toggle('active'); // Tambah atau hapus class active
+});
